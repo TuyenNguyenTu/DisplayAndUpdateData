9609e69849e1386f328ed01a8d38465e286536d0
AnimationCube <nguyentutuyen45@gmail.com>
2019-09-18 21:04:00 +0700

    first commit

---
+++ EmployeeController.cs
@@ -0,0 +1,58 @@
+﻿using LoadAndUpdateUsingJqueryAjax.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LoadAndUpdateUsingJqueryAjax.DAO;
+using System.Web.Script.Serialization;
+
+namespace LoadAndUpdateUsingJqueryAjax.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public JsonResult LoadData()
+        {
+            var dao = new EmployeeDAO();
+            var listDataEmployee = new List<tbl_Employee>();
+            var listEm = dao.GetListEmployees();
+            foreach (var item in listEm)
+            {
+                listDataEmployee.Add(new tbl_Employee()
+                {
+                    ID = item.ID,
+                    Name = item.Name,
+                    Age = item.Age,
+                    Status = item.Status
+                });
+            }
+            return Json(new
+            {
+                data = listDataEmployee,
+                status = true
+            },JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public JsonResult Update(string model)
+        {
+            // chuyển đổi 1 string sang
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            tbl_Employee employee = serializer.Deserialize<tbl_Employee>(model);
+
+            var listEm = new EmployeeDbContext();
+            var entity = listEm.tbl_Employee.Single(x => x.ID == employee.ID);
+            entity.Age = employee.Age;
+            listEm.SaveChanges();
+            return Json(new
+            {
+                status = true
+            });
+        }
+    }
+}
---
+++ controller/employeeController.js
@@ -0,0 +1,62 @@
+﻿var employeeController = {
+    init: function () {
+        employeeController.loadData();
+        employeeController.registerEvent();
+    },
+    // 
+    registerEvent: function () {
+        $('.txtAge').keypress(function (e) {
+            if (e.which == 13) {
+                var id = $(this).data('id');
+                var value = $(this).val();
+                employeeController.updateData(id, value);
+            }
+        })
+    },
+    updateData: function (id, value) {
+        var data = {
+            ID: id,
+            Age: value
+        };
+        $.ajax({
+            url: 'Update',
+            type: 'POST',
+            dataType: 'JSON',
+            //truyền lên bằng string
+            data: { model: JSON.stringify(data) },
+            success: function (response) {
+                if (response.status == true) {
+                    alert("Update Thành Công");
+                } else {
+                    alert("Update Không Thành Công");
+                }
+            }
+        })
+    },
+    loadData: function () {
+        $.ajax({
+            url: 'LoadData',
+            type: 'GET',
+            dataType: 'json',
+            success: function (response) {
+                if (response.status) {
+                    var data = response.data;
+                    var html = '';
+                    var temp = $('#dataTemp').html();
+                    $.each(data, function (i, item) {
+                        html += Mustache.render(temp, {
+                            ID: item.ID,
+                            Name: item.Name,
+                            Age: item.Age,
+                            Status: item.Status == true ? "<button type=\"button\" class=\"btn btn-success\">Active</button>" : "<button type=\"button\" class=\"btn btn-warning\">Locked</button>"
+                        });
+                    })
+                    $('#tblData').html(html);
+                    employeeController.registerEvent();
+                }
+            }
+
+        });
+    }
+}
+employeeController.init();
